52ac6cef52f76043271a38f6e1d27b94690689ad
bobcb <jcbbob@163.com>
2024-09-21 15:20:27 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -31,66 +31,40 @@ st.markdown("""
 
     /* Custom font size for the column headers */
     .small-header {
-        font-size: 22px;  /* Adjust the size for section headers */
+        font-size: 22px;  /* You can adjust this size as needed */
         font-weight: bold;
         padding-bottom: 10px; /* Adds some space below the title */
-    }
-
-    /* Custom font size for the feature labels */
-    .feature-label {
-        font-size: 20px;  /* Adjust the size for feature labels */
     }
     </style>
     """, unsafe_allow_html=True)
 
 # Layout the input fields in three columns with increased spacing between columns
-col1, spacer1, col2, spacer2, col3 = st.columns([1.2, 0.5, 1.2, 0.5, 1.2])
+col1, spacer1, col2, spacer2, col3 = st.columns([1.2,0.5, 1.2, 0.5, 1.2])
 
 with col1:
     st.markdown('<div class="small-header">S/S soil physicochemical properties</div>', unsafe_allow_html=True)
-    st.markdown('<span class="feature-label">SiO<sub>2</sub> (%)</span>', unsafe_allow_html=True)
-    feature1 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">CaO (%)</span>', unsafe_allow_html=True)
-    feature2 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Al<sub>2</sub>O<sub>3</sub> (%)</span>', unsafe_allow_html=True)
-    feature3 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Soil pH</span>', unsafe_allow_html=True)
-    feature4 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Soil heavy metal concentration (mg/kg)</span>', unsafe_allow_html=True)
-    feature5 = st.number_input('', step=0.01, format='%.2f')
+    feature1 = st.number_input(u'$\mathrm{SiO_2\;(\%)}$', step=0.01, format='%.2f')
+    feature2 = st.number_input(u'$\mathrm{CaO\;(\%)}$', step=0.01, format='%.2f')
+    feature3 = st.number_input(u'$\mathrm{Al_2O_3\;(\%)}$', step=0.01, format='%.2f')
+    feature4 = st.number_input(u'$\mathrm{Soil\;pH}$', step=0.01, format='%.2f')
+    feature5 = st.number_input(u'$\mathrm{Soil\;heavy\;metal}$\n$\mathrm{concentration\;(mg/kg)}$', step=0.01, format='%.2f')
 
 with col2:
     st.markdown('<div class="small-header">Experimental conditions</div>', unsafe_allow_html=True)
+    # Add spacing before inputs in this column
     st.markdown('<div class="spacing-row"></div>', unsafe_allow_html=True)
-    
-    st.markdown('<span class="feature-label">Temperature (℃)</span>', unsafe_allow_html=True)
-    feature6 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Curing time (d)</span>', unsafe_allow_html=True)
-    feature7 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Liquid/Solid</span>', unsafe_allow_html=True)
-    feature8 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Extraction agent pH</span>', unsafe_allow_html=True)
-    feature9 = st.number_input('', step=0.01, format='%.2f')
+    feature6 = st.number_input(u'$\mathrm{Temperature\;(℃)}$', step=0.01, format='%.2f')
+    feature7 = st.number_input(u'$\mathrm{Curing\;time\;(d)}$', step=0.01, format='%.2f')
+    feature8 = st.number_input(u'$\mathrm{Liquid/Solid}$', step=0.01, format='%.2f')
+    feature9 = st.number_input(u'$\mathrm{Extraction\;agent\;pH}$', step=0.01, format='%.2f')
 
 with col3:
     st.markdown('<div class="small-header">Heavy metal properties</div>', unsafe_allow_html=True)
-    
-    st.markdown('<span class="feature-label">Electronegativity</span>', unsafe_allow_html=True)
-    feature10 = st.number_input('', step=0.01, format='%.2f')
-    
-    st.markdown('<span class="feature-label">Hydrated ion radius (Å)</span>', unsafe_allow_html=True)
-    feature11 = st.number_input('', step=0.01, format='%.2f')
+    feature10 = st.number_input(u'$\mathrm{Electronegativity}$', step=0.01, format='%.2f')
+    feature11 = st.number_input(u'$\mathrm{Hydrated\;ion\;radius\;(Å)}$', step=0.01, format='%.2f')
 
 # Experimental heavy metal immobilization rate input
-st.markdown('<span class="feature-label">Experimental immobilization rate (%)</span>', unsafe_allow_html=True)
-feature = st.number_input('', step=0.01, format='%.2f')
+feature = st.number_input(u'$\mathrm{Experimental\;immobilization\;rate\;(\%)}$', step=0.01, format='%.2f')
 
 # Gather all feature inputs
 feature_values = [feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11]
